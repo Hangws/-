58dcb7a44ae026027602a916acd6b0ab097533ef
wenshi <13210753835@163.com>
2019-01-22 16:28:33 +0800

    更改项目名称等基本配置

--- javascript/method.js
+++ javascript/method.js
@@ -4,10 +4,11 @@ var str2 = "想你的心克制不住，不过是因为想你了。";
 var str3 = "守护着你的未来，因为我要参与其中。";
 var str4 = "阳光正好，你也正好。";
 var str5 = "希望我能给你一生幸福，让你此生安乐。";
-arr.push(str1,str2,str3,str4,str5)
+var str6 = "以梦为马,诗酒趁年华."
+arr.push(str1,str2,str3,str4,str5,str6)
 // 展示文字
 function showText(event){
-  var random = Math.floor(Math.random()*arr.length);  //可均衡获取0到9的随机整数。
+  var random = Math.floor(Math.random()*arr.length);  //可均衡获取0到数组长度的的随机整数。
   $(event).text(arr[random]);
 }
 // 显示小组件
